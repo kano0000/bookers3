dd35c6c515636daa2047da6cc5097f072263e221
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2023-12-29 05:00:28 +0000

    [Create]model,controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+end
