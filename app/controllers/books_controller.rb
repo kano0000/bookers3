a32e8260400a2e4e970e8bb4ec6a56048bfeba6c
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2023-12-29 10:05:11 +0000

    [Create]book-indexページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,37 @@
 class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  
   def show
   end
 
   def edit
   end
 
-  def index
-  end
-
   def update
-  end
-
-  def create
   end
 
   def destroy
   end
-
-  def new
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
+
 end
