54efd19458c1e66919ac03d9642298c3710daaaa
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2024-01-01 07:01:29 +0000

    [Create]chat-controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,16 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
    # フォローしている関連付け
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  
   # フォローされている関連付け
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
   # フォローしているユーザーを取得
   has_many :followings, through: :active_relationships, source: :followed
-  
   # フォロワーを取得
   has_many :followers, through: :passive_relationships, source: :follower
+  # チャット機能
+  has_many :user_rooms, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_many :rooms, through: :user_rooms
   
   has_one_attached :profile_image
   
