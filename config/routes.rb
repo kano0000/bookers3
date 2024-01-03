7ed8dd145a19a54c37db798afe13cbce4c81f67c
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-01-03 05:21:19 +0000

    [Create]投稿数検索

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update] do
+    get "posts_on_date" => "users#posts_on_date"
     resource :relationships, only: [:create, :destroy]
   	  get "followings" => "relationships#followings", as: "followings"
   	  get "followers" => "relationships#followers", as: "followers"
