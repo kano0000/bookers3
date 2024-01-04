f9cbe44112a72b2b9aac9da442c0eff2367b8371
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-01-04 01:26:01 +0000

    [Create]アソシエーション

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,15 @@
 class Group < ApplicationRecord
+  has_many :group_user, dependent: :destroy
+  belongs_to :owner, class_name: 'User'
+  has_many :users, through: :group_users
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  has_one_attached :group_image
+  
+  def is_owned_by?(user)
+    owner_id == user.id   
+    # owner.id
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
   has_many :rooms, through: :user_rooms
+  # グループ機能
+  has_many :group_user, dependent: :destroy
   
   has_one_attached :profile_image
   
