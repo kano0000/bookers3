b7f908727e5254b97c057cbe332f283144bc2c1c
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-01-05 02:28:24 +0000

    [Create]Group-Index画面

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,4 +11,12 @@ class Group < ApplicationRecord
     owner.id == user.id   
   end
   
+  def get_group_image(width,height)
+    unless group_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      group_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')      
+    end
+    group_image.variant(resize_to_limit: [width,height]).processed
+  end
+  
 end
