7fbabb2377ab59dfc4ef671e974956783dfb73b1
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-01-05 07:52:26 +0000

    [Update]グループメンバー

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -8,7 +8,7 @@ class GroupUsersController < ApplicationController
   end
   
   def destroy
-    group_user = current_user.group_users.find_by(group_id: params[group_id])
+    group_user = current_user.group_users.find_by(group_id: params[:group_id])
     group_user.destroy
     redirect_to request.referer
   end
