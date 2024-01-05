5e0f0c1e0f11bdab0ad21f93278872c60bb4380b
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-01-05 02:59:45 +0000

    [Create]Group-Edit画面

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -33,7 +33,7 @@ class GroupsController < ApplicationController
   end
   
   def update
-    @group =Gruop.find(params[:id])
+    @group =Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path
     else
